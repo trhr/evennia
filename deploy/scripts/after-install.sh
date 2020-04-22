2f0b02dc6bcbf4bbd5febf82ce7678570e9ffff8
trhr <sage@limitlessinteractive.com>
2020-04-22 17:18:27 -0500

    AppSpec

---
+++ deploy/scripts/after-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /tmp/deploy/
+
+echo "The Evennia AfterInstall deployment lifecycle event successfully completed." > after-install.txt
---
+++ deploy/scripts/validate-service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /tmp/CodeDeployExample
+
+echo "The Evennia ValidateService deployment lifecycle event successfully completed." > validate-service.txt
+
+unset FOLDER
