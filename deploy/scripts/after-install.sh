aedae15e57211968cd9e67c82c265eef08f462d6
trhr <sage@limitlessinteractive.com>
2020-05-29 21:51:00 -0500

    Updated requirements

--- deploy/scripts/after-install.sh
+++ deploy/scripts/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/evennia/
-pip3 install --upgrade -r requirements.txt
+sudo pip3 install --upgrade -r requirements.txt
 echo "The Evennia AfterInstall deployment lifecycle event successfully completed." > /tmp/deploy/after-install.txt
 
 
